36cdd01396b4daff05841eca58170ba47f059607
litoy-ko <litoys.ark2nd@gmail.com>
2024-03-31 11:09:50 +0800

    initial commit
    HW8 initial commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Described in documentation;
+
+import SimpleLightbox from "simplelightbox";
+// Additional styles import
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector("ul.gallery");
+
+function createGalleryItem(array) {
+	return array
+		.map(({ preview, original, description }) => {
+			return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}"/>
+            </a>
+        `;
+		})
+		.join("");
+}
+
+const photosMarkup = createGalleryItem(galleryItems);
+galleryContainer.insertAdjacentHTML("beforeend", photosMarkup);
+
+const galleryHandler = new SimpleLightbox(".gallery a", { captionsData: "alt", captionDelay: 250 });
+galleryHandler.on("show.simplelightbox");
